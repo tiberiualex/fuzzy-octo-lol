a6a18d22fb5bfddb21891d4ed0ae54dbcd6bfee2
tiberiualex <tiberiu.alexander@gmail.com>
2016-04-20 21:20:54 +0100

    Add mock data and import ramda

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,7 @@
-console.log('fuzzy-octo-lol');
+import r from 'ramda';
+import products from '../mock-data/products.json';
+
+const getBrands = r.compose(r.uniq, r.map(r.prop('brand')));
+const brands = getBrands(products);
+
+console.log(brands);
