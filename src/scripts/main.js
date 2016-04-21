17b669bda17c3ec192fd7f0d6d6fe4bdfe369af6
tiberiualex <tiberiu.alexander@gmail.com>
2016-04-21 20:46:54 +0100

    Add functions for sorting and finding featured products

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,11 @@ import r from 'ramda';
 import products from '../mock-data/products.json';
 
 const getBrands = r.compose(r.uniq, r.map(r.prop('brand')));
-const brands = getBrands(products);
+const sortByPriceAsc = r.sortBy(r.prop('price'));
+const sortByPriceDesc = r.compose(r.reverse, sortByPriceAsc);
+const isFeatured = r.propEq('featured', true);
+const getFeatured = r.filter(isFeatured);
 
-console.log(brands);
+const featured = getFeatured(products);
+
+console.log(featured);
