ce652b856373936a5186774a4885c287d751d9dc
geduardcatalin <g.eduardcatalin@gmail.com>
2016-05-03 23:35:12 +0300

    Close menu when overlay is clicked

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -7,16 +7,19 @@ const Menu = function Menu(selector) {
 
 Menu.prototype.init = function() {
   this.menuLinks = this.element.querySelector('.js-menu__links');
+  this.overlay = document.querySelector('.overlay');
   this.addListeners();
 };
 
 Menu.prototype.addListeners = function() {
   const menuButton = this.element.querySelector('.js-menu__trigger');
   menuButton.addEventListener('click', this.toggleMenu.bind(this));
+  this.overlay.addEventListener('click', this.toggleMenu.bind(this));
 };
 
 Menu.prototype.toggleMenu = function() {
   this.menuLinks.classList.toggle('menu--active');
+  this.overlay.classList.toggle('overlay--active');
 };
 
 export default Menu;
